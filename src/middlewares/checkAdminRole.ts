2c2c4ebdeed90b0958e5e4b3a8f5794c64b1a4e9
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2025-04-09 14:05:59 +0200

    user routes updated with auth

--- src/middlewares/checkAdminRole.ts
+++ src/middlewares/checkAdminRole.ts
@@ -1,12 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
-
+import { User } from '../models/User.js';
 export const checkAdminRole = (req: Request, res: Response, next: NextFunction): void => {
-  const user = req.user as { role: string };
-  // console.log(user)
+  const user = req.user as User;
+  // console.log(user);
   if (!user || user.role !== 'administrator') {
     res.status(403).json({ error: 'Forbidden: Only administrators can perform this action' });
     return;
   }
 
   next();
+};
+export const checkSelfOrAdmin = (req: Request, res: Response, next: NextFunction):void => {
+  const user = req.user as User;
+    console.log(user);
+  const { id } = req.params;
+    console.log(id);
+  if (user?.role === 'administrator' || user?._id === id) {
+    return next();
+  }
+  res.status(403).json({ message: 'You can update only your own account' });
+  return;
 };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,15 +6,15 @@ import {
   getAllUsersController,
   updateUserDetailsController,
 } from '../controllers/userController.js';
-import { checkAdminRole } from '../middlewares/checkAdminRole.js';
+import { checkAdminRole, checkSelfOrAdmin } from '../middlewares/checkAdminRole.js';
 import { authenticateJWT } from '../middlewares/authMiddleware.js';
 const router = Router();
-router.get('/getall', getAllUsersController);
+router.get('/getall', authenticateJWT, getAllUsersController);
 
 router.post('/create',authenticateJWT,checkAdminRole, createUserController);
-router.put('/update/:id', updateUserDetailsController);
-router.get('/:id', findUserByIdController);
+router.put('/update/:id', authenticateJWT,checkSelfOrAdmin, updateUserDetailsController);
+router.get('/:id', authenticateJWT, findUserByIdController);
 
-router.delete('/:id', deleteUserController);
+router.delete('/:id', authenticateJWT, deleteUserController);
 
 export default router;
