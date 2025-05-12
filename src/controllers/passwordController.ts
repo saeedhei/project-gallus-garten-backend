236ef8cdaba1240b6315e3ed3b7d8c189d4843a8
KeyError34 <150452663+KeyError34@users.noreply.github.com>
2025-05-12 17:54:53 +0200

    changePasswordLogic rewritten to class

--- src/controllers/passwordController.ts
+++ src/controllers/passwordController.ts
@@ -1,32 +1,36 @@
 import { Request, Response } from 'express';
-import { requestPasswordReset, resetPassword } from '../services/passwordServise.js';
+import { PasswordService } from '../services/passwordServise.js';
 
-export const handleRequestReset = async (req: Request, res: Response): Promise<void> => {
-  try {
-    const { email } = req.body;
-    if (!email) {
-      res.status(400).json({ error: 'Login or email is required' });
-      return
+export class PasswordController {
+  private passwordService = new PasswordService();
+
+  public requestReset = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { email } = req.body;
+      if (!email) {
+        res.status(400).json({ error: 'Login or email is required' });
+        return;
+      }
+
+      await this.passwordService.requestPasswordReset(email);
+      res.json({ message: 'Password reset link sent to email' });
+    } catch (error) {
+      res.status(500).json({ error: (error as Error).message });
     }
+  };
 
-    await requestPasswordReset(email);
-    res.json({ message: 'Password reset link sent to email' });
-  } catch (error) {
-    res.status(500).json({ error: (error as Error).message });
-  }
-};
+  public resetPassword = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { token, newPassword } = req.body;
+      if (!token || !newPassword) {
+        res.status(400).json({ error: 'Token and new password are required' });
+        return;
+      }
 
-export const handleResetPassword = async (req: Request, res: Response):Promise<void> => {
-  try {
-    const { token, newPassword } = req.body;
-    if (!token || !newPassword) {
-      res.status(400).json({ error: 'Token and new password are required' });
-      return
+      await this.passwordService.resetPassword(token, newPassword);
+      res.json({ message: 'Password has been reset successfully' });
+    } catch (error) {
+      res.status(500).json({ error: (error as Error).message });
     }
-
-    await resetPassword(token, newPassword);
-    res.json({ message: 'Password has been reset successfully' });
-  } catch (error) {
-    res.status(500).json({ error: (error as Error).message });
-  }
-};
+  };
+}
